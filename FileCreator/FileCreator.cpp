86b1da01bd154b7f836d09d7a5e10dafce4603fd
praveer1981 <yadav.praveer.kumar@gmail.com>
2022-03-24 15:53:17 +0530

    Adeed code to call wkix32.exe as a child process

--- FileCreator/FileCreator.cpp
+++ FileCreator/FileCreator.cpp
@@ -1,21 +1,26 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <Shlwapi.h>
+#include < strsafe.h>
 #pragma comment(lib, "shlwapi")
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <tchar.h>
 
+#define _CREATE_FILE_ 1
 
 int main()
 {
 
-    char szExePath[MAX_PATH]{}, szHandle[64]{}, szCmdLine[512]{};
+    char szExePath[MAX_PATH]{}, szKixFilePath[MAX_PATH]{}, szHandle[64]{}, szCmdLine[512]{};
     GetModuleFileNameA(NULL, szExePath, _countof(szExePath));
     PathRemoveFileSpecA(szExePath); // Removes application name Ex: xyz.exe
-    PathAppendA(szExePath, "PraveerTest.txt"); // Append the file name in the same path
 
+    PathAppendA(szExePath, "PraveerTemp.kix"); // Append the file name in the same path
+    STARTUPINFOA si{ sizeof si };
+    PROCESS_INFORMATION pi{};
+#ifdef _CREATE_FILE_
     SECURITY_ATTRIBUTES sa{ sizeof sa, nullptr, TRUE };
     HANDLE hFile = CreateFileA(szExePath,
                                 GENERIC_READ | GENERIC_WRITE,
@@ -25,7 +30,7 @@ int main()
                                 FILE_ATTRIBUTE_NORMAL, NULL); // Create a file
     if (hFile != INVALID_HANDLE_VALUE)
     {
-        char szLines[] = "This is the test application to test file locking mechanism \n";
+        char szLines[] = ";--------start PraveerTemp.kix--------\n 'Hello World ' \n;-------- end PraveerTemp.kix--------";
         printf_s("Write the string in the test file:\n%s", szLines);
         DWORD dwWritten{};
         if (WriteFile(hFile,
@@ -34,15 +39,26 @@ int main()
             &dwWritten,
             NULL)) // Write into the file "PraveerTest.txt"
         {
-            STARTUPINFOA si{ sizeof si };
-            PROCESS_INFORMATION pi{};
+
+#endif
+            HRESULT result = StringCchCopyA(szKixFilePath, sizeof(szKixFilePath), szExePath);
+
+            if (result != S_OK)
+            {
+                printf_s("copy string failed, error %d\n", GetLastError());
+            }
+
             PathRemoveFileSpecA(szExePath); // again remove the "PraveerTest.txt" from the path
-            PathAppendA(szExePath, "FileReader.exe"); // I want to FileReader.exe in the path; 
-
+            PathAppendA(szExePath, "wKiX32.exe"); // I want to wKIX32.exe to run as a child process.
+#if _CREATE_FILE_
             UINT_PTR uiHandle = reinterpret_cast<UINT_PTR>(hFile);
-
-            sprintf_s(szCmdLine, "\"%s\" %Iu", szExePath, uiHandle);
+            sprintf_s(szCmdLine, "\"%s\" \"%s\" %Iu", szExePath, szKixFilePath, uiHandle);
+#else
+            sprintf_s(szCmdLine, "\"%s\" \"%s\"", szExePath, szKixFilePath);
+#endif
+           // sprintf_s(szCmdLine, "\"%s\"", szExePath);
             // Here we are creating the child process - FileReader.exe it will read the dta from "PraveerTest.txt" file
+
             if (CreateProcessA(nullptr,
                 szCmdLine, 
                 nullptr,
@@ -59,6 +75,7 @@ int main()
             }
             else
                 printf_s("CreateProcess failed, error %d\n", GetLastError());
+#ifdef _CREATE_FILE_
         }
         else
             printf_s("Writefile failed, error %d\n", GetLastError());
@@ -67,6 +84,6 @@ int main()
     }
     else
         printf_s("CreateFile failed, error %d\n", GetLastError());
-
+#endif
     return 0;
 }
